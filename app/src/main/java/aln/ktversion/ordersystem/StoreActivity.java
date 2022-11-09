f8fac3925c47eda2b066b9192364723f71d060b9
W.Allen <whosaty@gmail.com>
2022-11-09 16:30:22 +0800

    Add store layout

--- app/src/main/java/aln/ktversion/ordersystem/StoreActivity.java
+++ app/src/main/java/aln/ktversion/ordersystem/StoreActivity.java
@@ -1,14 +1,27 @@
 package aln.ktversion.ordersystem;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class StoreActivity extends AppCompatActivity {
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_store);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigation_store);
+        NavHostFragment navHostFragment =
+                (NavHostFragment) getSupportFragmentManager()
+                        .findFragmentById(R.id.fragmentContainerView_store);
+        NavController navController = navHostFragment.getNavController();
+        NavigationUI.setupWithNavController(bottomNavigationView,navController);
     }
 }
